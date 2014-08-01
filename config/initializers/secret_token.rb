cd1152f282fccf97697ecd5dc54ef215ee89aab9
Tin <mighty_mouse90210@hotmail.com>
2014-08-01 11:19:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TinApp::Application.config.secret_key_base = '449b69b4032a4d09da5f9bc51c58695f048b874ba1b3d7a8f9cc6b0e57b48ef8b9f1d721707400754a93f1de24431d0a0944cdb7073e09e90ea6fdca9f8a92f9'
